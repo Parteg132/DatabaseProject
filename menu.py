6465657ee59271304852d5e792b9df2b96696bf4
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-12-21 18:28:22 +0100

    Added menu

---
+++ menu.py
@@ -0,0 +1,41 @@
+def print_menu():
+  print("1. Option 1")
+  print("2. Option 2")
+  print("3. Option 3")
+  print("4. Option 4")
+  print("5. Option 5")
+  print("6. Option 6")
+  print("7. Option 7")
+  print("8. Option 8")
+  print("9. Option 9")
+  print("0. Quit")
+
+def main():
+  while True:
+    print_menu()
+    choice = input("Enter your choice: ")
+    if choice == "1":
+      # Code for Option 1
+    elif choice == "2":
+      # Code for Option 2
+    elif choice == "3":
+      # Code for Option 3
+    elif choice == "4":
+      # Code for Option 4
+    elif choice == "5":
+      # Code for Option 5
+    elif choice == "6":
+      # Code for Option 6
+    elif choice == "7":
+      # Code for Option 7
+    elif choice == "8":
+      # Code for Option 8
+    elif choice == "9":
+      # Code for Option 9
+    elif choice == "0":
+      break
+    else:
+      print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+  main()
